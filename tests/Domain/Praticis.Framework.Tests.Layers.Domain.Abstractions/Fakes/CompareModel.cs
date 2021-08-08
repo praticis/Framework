477c37ebae51a697c7b78841187530de55d8281b
Vinícius Lima <vinicius.lima@praticis.com>
2021-08-08 18:27:22 -0300

    Reorganized Praticis.Framework.Layers.Domain.Abstractions project folders.

---
+++ tests/Domain/Praticis.Framework.Tests.Layers.Domain.Abstractions/Fakes/CompareModel.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+using Praticis.Framework.Layers.Domain.Abstractions;
+
+namespace Praticis.Framework.Tests.Layers.Domain.Abstractions.Fakes
+{
+    public class CompareModel : BaseModel
+    {
+        public void ChangeId(Guid id)
+            => this.Id = id;
+    }
+}
